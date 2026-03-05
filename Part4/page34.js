5c26a29426137cd0297a0f1fcea420bb916bb557
Devi Jetta <jettadevi289@gmail.com>
2026-03-05 19:17:05 +0530

    commit

--- Part4/page34.js
+++ Part4/page34.js
@@ -1,10 +1,10 @@
 import express from "express";
 const app = express();
 app.set("view engine" , "ejs");
-app.set("views" , "views") // all the views in views folder
-app.listen(8081 , () => console.log("sever started"));
+app.set("views" , "./views") // all the views in views folder
+app.listen(8080 , () => console.log("sever started"));
 app.get("/" , (req,res) => {
-    // res.render("login")
-    res.render("register")
+    res.render("login")
+    // res.render("register")
 
 })
