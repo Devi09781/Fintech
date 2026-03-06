586fabc6015286916c510e4881835920d9e07b55
Devi Jetta <jettadevi289@gmail.com>
2026-03-06 11:24:50 +0530

    commit

--- Part4/page34.js
+++ Part4/page34.js
@@ -1,10 +1,29 @@
-import express from "express";
+ import express from "express";
 const app = express();
-app.set("view engine" , "ejs");
-app.set("views" , "./views") // all the views in views folder
-app.listen(8080 , () => console.log("sever started"));
-app.get("/" , (req,res) => {
-    res.render("login")
-    // res.render("register")
+app.set("view engine","ejs")
+app.set("views","views")
+app.listen(8080, () => console.log("Server Started"));
 
+const users = [
+  { id : 1 ,name : "devi" , email : "devi123@gmail.com" , password : "1234"},
+    {id : 2 ,name : "nani" , email : "nanii123@gmail.com" , password : "1234"} ,
+      {id : 3 ,name : "ram" , email : "ram123@gmail.com" , password : "1234"}
+
+
+];
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.post("./login" , (req , res) =>{
+  res.redirect("/")
+  
 })
+
+  app.get("/register" , (req, res) => {
+    res.render("register");
+});
+
+app.get("/", (req, res) => {
+    res.render("dashboard" ,{users}); //getting data which is existed
+});
+
